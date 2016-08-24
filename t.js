52797741fbc45bb96eecf097631f16f47ff61d84
harry <411981379@qq.com>
2016-08-24 11:23:30 +0800

    1

--- t.js
+++ t.js
@@ -2,7 +2,7 @@
 global.fs = require ('fs');
 var path = require('path');
 
-var folderPath = 'E:\\winsun\\src'
+var folderPath = '/home/harry/Documents/winsun-electron/node_modules'
 var uploadObj = {
 	status: '准备',
 	data: {
@@ -30,40 +30,38 @@ traverse(folderPath, uploadObj.data.children, err => {
 })
 
 function traverse(filePath, position, callback ) {
-	fs.readdir(filePath, (err, entries) => {
-				if (entries.length == 0) {
-					return callback(null)
+	fs.stat(filePath, (err, stat) => {
+		if (err || (!stat.isDirectory() && !stat.isFile())) {
+			return callback(err||'error')
+		}
+		uploadObj.count++
+		if (stat.isFile()) {
+			console.log('count : ' + uploadObj.count + ' ' + filePath + ' ----> file');
+			return callback();
+		}
+		console.log('count : ' + uploadObj.count + ' ' + filePath + ' ----> directory');
+
+		fs.readdir(filePath, (err, entries) => {
+			if (entries.length == 0) {
+				return callback(null)
+			}
+
+			let count = entries.length
+			let index = 0
+			// position.push({times: 0,children: [],path: path.join(filePath,entries[index]),status: '准备',parent: null,type: stat.isFile()?'file':'folder',name: entries[index]})
+			let childrenCallback = err => {
+				if (err) {
+					return callback(err)
 				}
-				let count = entries.length
-				let index = 0
-					fs.stat(filePath, (err, stat) => {
-						if (err || (!stat.isDirectory() && !stat.isFile())) {
-							return callback(err||'error')
-						}
-						uploadObj.count++
-						if (stat.isFile()) {
-							console.log('count : ' + uploadObj.count + ' ' + filePath + ' ----> file');
-							position.push({times: 0,children: [],path: filePath,status: '准备',parent: null,type: 'file',name: path.basename(filePath)})
-							return callback(null)
-						}else {
-							console.log('count : ' + uploadObj.count + ' ' + filePath + ' ----> directory');
-							position.push{times: 0,children: [],path: filePath,status: '准备',parent: null,type: 'folder',name: path.basename(filePath)}
-							let childrenCallback = err => {
-								if (err) {
-									return callback(err)
-								}
-								index++
-								if (index == count) {
-									callback(null)
-								}else {
-									traverse(path.join(filePath,entries[index],position),position[index].children)
-								}
-							}
-							traverse(path.join(filePath,entries[index]),position[index].children)
-						}
-					})
-
-				
-			})
-
+				index++
+				if (index == count) {
+					callback(null)
+				}else {
+					traverse(path.join(filePath,entries[index]),position,childrenCallback)
+				}
+			}
+			traverse(path.join(filePath,entries[index]),position,childrenCallback)
+		})
+		
+	})
 }
