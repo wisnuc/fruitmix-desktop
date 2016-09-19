60d0abbe5326328d61a590c32eefc618cf959260
zengwenbing <fiveSecondsRealMan@163.cn>
2016-09-19 18:25:54 +0800

    initial menugroup commit

---
+++ src/app/actions/action-v2.js
@@ -0,0 +1,14 @@
+/**
+  v0.2.0
+**/
+
+const v2Actions = {
+	initialMenuGroup(menuItems) {
+	  return {
+			type: 'INITIAL_MENUGROUP',
+			menuItems
+		};
+	}
+};
+
+export default v2Actions;
---
+++ src/app/reducers/navigation-v2.js
@@ -0,0 +1,5 @@
+/**
+  v0.2.0
+**/
+
+import { combineReducers } from 'redux';
