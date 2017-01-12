aac592189327259dc81540c1fef5d45be3ec27d8
matianfu <matianfu@gmail.com>
2017-01-12 17:31:53 +0800

    tmp

---
+++ src/app/components/common/FlatButton.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { FlatButton } from 'material-ui'
+
+export default props => <FlatButton {...props} labelStyle={{verticalAlign: undefined}} />
+
+
---
+++ src/app/components/login/FirstUserBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class FirstUserBox extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      expanded: false
+    }
+  }
+
+  render() {
+    return (
+    )
+  }
+}
