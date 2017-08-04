c1706233875f5c66918ce8cfb47d051b832c4f78
lixinwei <xinwei.li@winsuntech.cn>
2017-08-04 16:01:30 +0800

    store hash in win32

--- node/lib/xattr.js
+++ node/lib/xattr.js
@@ -1,8 +1,29 @@
 const Promise = require('bluebird')
+const os = require('os')
 const fs = Promise.promisifyAll(require('fs'))
-const xattr = Promise.promisifyAll(require('fs-xattr'))
 
 const FRUITMIX = 'user.fruitmix'
+
+let xattr
+try {
+  if (os.platform() === 'linux' || os.platform() === 'darwin') {
+    console.log('os.platform', os.platform())
+    xattr = Promise.promisifyAll(require('fs-xattr'))
+  } else if (os.platform() === 'win32') {
+    xattr = {
+      getAsync: async (target, string) => {
+        const data = await fs.readFileAsync(`${target}:${string}`, { encoding: 'utf-8' })
+        console.log('read xattr', data)
+        return data
+      },
+      setAsync: async (target, string, attr) => {
+        await fs.writeFileAsync(`${target}:${string}`, JSON.stringify(attr))
+      }
+    }
+  }
+} catch (e) {
+  console.log('load fs.xattr error', e)
+}
 
 const readXattrAsync = async (target) => {
   let attr
@@ -12,8 +33,10 @@ const readXattrAsync = async (target) => {
     /* may throw xattr ENOENT or JSON SyntaxError */
     console.log('readXattrAsync error: ', e.code || e)
   }
+  if (typeof attr === 'string') attr = JSON.parse(attr)
   const stats = await fs.lstatAsync(target)
-  if (attr && attr.htime && attr.htime === stats.mtime.getTime()) return attr
+  const htime = os.platform() === 'win32' ? stats.atime.getTime() : stats.mtime.getTime()
+  if (attr && attr.htime && attr.htime === htime) return attr
   return null
 }
 
@@ -23,12 +46,12 @@ const readXattr = (target, callback) => {
 
 const setXattrAsync = async (target, attr) => {
   const stats = await fs.lstatAsync(target)
-  const htime = stats.mtime.getTime()
+  const htime = os.platform() === 'win32' ? stats.atime.getTime() : stats.mtime.getTime()
   const newAttr = Object.assign({}, attr, { htime })
   try {
     await xattr.setAsync(target, FRUITMIX, JSON.stringify(newAttr))
   } catch (e) {
-    console.log('setXattrAsync error: ', e.code)
+    console.log('setXattrAsync error: ', e.code || e)
   }
   return newAttr
 }
