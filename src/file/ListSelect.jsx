fb71a2fb1b16e3ae2174da319400ad0b0e78a069
lixinwei <xinwei.li@winsuntech.cn>
2017-10-25 16:13:04 +0800

    add shift and ctrl feature to selectBox

--- src/file/ListSelect.jsx
+++ src/file/ListSelect.jsx
@@ -37,9 +37,19 @@ class ListSelect extends EventEmitter {
     return this.state
   }
 
-  addByArray(array) {
-    const set = new Set([...array, ...this.state.selected])
-    this.setState({ selected: this.state.shift ? [...set] : array })
+  addByArray(array, session) {
+    console.log('addByArray', array, session, this.state.ctrl, this.state.shift)
+    if (this.state.shift) {
+      const set = new Set([...array, ...this.state.selected])
+      this.setState({ selected: [...set] })
+    } else if (this.state.ctrl) {
+      const isSameBox = this.session === session
+      this.session = session
+      if (!isSameBox) this.preArray = this.state.selected
+      const set = new Set([...array, ...this.preArray])
+      array.forEach(i => this.preArray.includes(i) && set.delete(i))
+      this.setState({ selected: [...set] })
+    } else this.setState({ selected: array })
   }
 
   keyEvent(ctrl, shift) {
@@ -66,11 +76,7 @@ class ListSelect extends EventEmitter {
   // toggle select and (sort of) specified
   ctrlLeftClick(index) {
     if (index === -1) { // click outside
-      this.setState({
-        selected: [],
-        specified: -1,
-        hover: -1
-      })
+      // this.setState({ selected: [], specified: -1, hover: -1 })
     } else {
       const idx = this.state.selected.indexOf(index)
       if (idx !== -1) {
@@ -92,8 +98,9 @@ class ListSelect extends EventEmitter {
   shiftLeftClick(index) {
     const { specified, selected } = this.state
 
-    if (index === -1) // click outside
-    { this.setState({ selected: [], specified: -1, hover: -1 }) } else if (specified === -1) {
+    if (index === -1) { // click outside
+      // this.setState({ selected: [], specified: -1, hover: -1 })
+    } else if (specified === -1) {
       if (!selected.includes(index)) {
         this.setState({ selected: [...selected, index], specified: index })
       } else {
