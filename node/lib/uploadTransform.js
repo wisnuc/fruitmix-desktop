7b2f25eae9eccd183cda68e79c099d4f18e2bddc
lixinwei <xinwei.li@winsuntech.cn>
2017-11-17 17:26:22 +0800

    fast readdir

--- node/lib/uploadTransform.js
+++ node/lib/uploadTransform.js
@@ -78,23 +78,22 @@ class Task {
       concurrency: 4,
       transform(x, callback) {
         const read = async (entries, dirUUID, driveUUID, policies, task) => {
-          const files = []
           for (let i = 0; i < entries.length; i++) {
-            // if (task.paused) throw Error('task paused !')
             if (task.paused) break
-            const policy = policies[i]
             const entry = entries[i]
             const stat = await fs.lstatAsync(path.resolve(entry))
             const fullName = path.parse(entry).base
+
             if (fullName === '.DS_Store' && !stat.isDirectory()) {
               task.warnings.push(Object.assign({
-                pipe: 'readDir', entry, error: { code: 'EDSSTORE' }, policy, stat, task: task.uuid, type: 'file'
+                pipe: 'readDir', entry, error: { code: 'EDSSTORE' }, stat, task: task.uuid, type: 'file'
               }))
               continue
             }
+
             if (!stat.isFile() && !stat.isDirectory()) {
               task.warnings.push(Object.assign({
-                pipe: 'readDir', entry, error: { code: 'ETYPE' }, policy, stat, task: task.uuid
+                pipe: 'readDir', entry, error: { code: 'ETYPE' }, stat, task: task.uuid
               }))
               console.log('unsupport type', entry)
               continue
@@ -103,13 +102,48 @@ class Task {
             const type = stat.isDirectory() ? 'directory' : 'file'
             if (fullName !== sanitize(fullName)) {
               task.warnings.push(Object.assign({
-                pipe: 'readDir', entry, error: { code: 'ENAME' }, policy, stat, task: task.uuid, type
+                pipe: 'readDir', entry, error: { code: 'ENAME' }, stat, task: task.uuid, type
               }))
               console.log('invalid name:', entry)
               continue
             }
 
             task.count += 1
+
+            if (stat.isDirectory()) {
+              /* read child */
+              const children = await fs.readdirAsync(path.resolve(entry))
+              const newEntries = []
+              children.forEach(c => newEntries.push(path.join(entry, c)))
+              await read(newEntries, dirUUID, driveUUID, policies, task)
+            } else {
+              task.size += stat.size
+            }
+          }
+          return ({ entries, dirUUID, driveUUID, policies, task })
+        }
+        const { entries, dirUUID, driveUUID, policies, task } = x
+        read(entries, dirUUID, driveUUID, policies, task).then(y => callback(null, y)).catch(callback)
+      }
+    })
+
+    this.mkdir = new Transform({
+      name: 'mkdir',
+      concurrency: 4,
+      transform(x, callback) {
+        const read = async (entries, dirUUID, driveUUID, policies, task) => {
+          const files = []
+          for (let i = 0; i < entries.length; i++) {
+            // if (task.paused) throw Error('task paused !')
+            if (task.paused) break
+            const policy = policies[i]
+            const entry = entries[i]
+            const stat = await fs.lstatAsync(path.resolve(entry))
+            const fullName = path.parse(entry).base
+
+            if (fullName === '.DS_Store' && !stat.isDirectory()) continue
+            if (!stat.isFile() && !stat.isDirectory()) continue
+            if (fullName !== sanitize(fullName)) continue
 
             if (stat.isDirectory()) {
               /* create fold and return the uuid */
@@ -128,11 +162,9 @@ class Task {
               childPolicies.length = newEntries.length
               childPolicies.fill({ mode: policy.mode }) // !!! fill with one object, all shared !!!
               if (policy.mode === 'rename' || policy.mode === 'replace') childPolicies.fill({ mode: 'normal' })
-              // debug('childPolicies', childPolicies)
+
               if (task.paused) break
               this.push({ entries: newEntries, dirUUID: uuid, driveUUID, policies: childPolicies, task })
-            } else {
-              task.size += stat.size
             }
             files.push({ entry, stat, policy })
           }
@@ -391,7 +423,7 @@ class Task {
       }
     })
 
-    this.readDir.pipe(this.hash).pipe(this.diff).pipe(this.upload)
+    this.readDir.pipe(this.mkdir).pipe(this.hash).pipe(this.diff).pipe(this.upload)
 
     this.readDir.on('data', (x) => {
       const { dirUUID, task } = x
@@ -428,7 +460,7 @@ class Task {
 
       const preLength = this.errors.length
       this.errors.length = 0
-      const pipes = ['readDir', 'hash', 'diff', 'upload']
+      const pipes = ['readDir', 'mkdir', 'hash', 'diff', 'upload']
       pipes.forEach((p) => {
         if (!this[p].failed.length) return
         this[p].failed.forEach((x) => {
