0eda62cbdee92264a2cdefc7100556f2498cfa2a
lixinwei <xinwei.li@winsuntech.cn>
2017-12-21 15:48:33 +0800

    fix bug of db.js

--- node/lib/db.js
+++ node/lib/db.js
@@ -12,6 +12,25 @@ a custom db to store transmisson data
 class db {
   constructor(dir) {
     this.dir = path.resolve(dir)
+    this.store = {}
+    this.WIP = {}
+    this.next = {}
+
+    this.write = (id) => {
+      this.WIP[id] = true
+      this.next[id] = false
+      const { data, cb } = this.store[id]
+      const tmp = path.join(this.dir, `${uuid.v4()}~`)
+      const target = path.join(this.dir, id)
+      fs.writeFile(tmp, JSON.stringify(data), (err) => {
+        if (err) cb(err)
+        else fs.rename(tmp, target, err => {
+          this.WIP[id] = false
+          cb(err)
+          if (this.next[id]) this.write(id)
+        })
+      })
+    }
   }
 
   initialize(cb) {
@@ -19,12 +38,9 @@ class db {
   }
 
   save(id, data, cb) {
-    const tmp = path.join(this.dir, `${uuid.v4()}~`)
-    const target = path.join(this.dir, id)
-    fs.writeFile(tmp, JSON.stringify(data), (err) => {
-      if (err) cb(err)
-      else fs.rename(tmp, target, cb)
-    })
+    this.store[id] = { data, cb }
+    if (!this.WIP[id]) this.write(id)
+    else this.next[id] = true
   }
 
   load(id, cb) {
@@ -45,8 +61,14 @@ class db {
       const list = []
       if (!dir.length) return cb(null, list)
       let count = dir.length
-      dir.forEach(id => this.load(id, (error, data) => {
-        if (error || id.length !== 36) this.remove(id, e => console.log('remove error data', id, 'error', e))
+      const newDir = dir.filter(id => {
+        if (id.length === 36) return true
+        this.remove(id, e => console.log('remove error data', id, 'error', e))
+        return false
+      })
+
+      newDir.forEach(id => this.load(id, (error, data) => {
+        if (error) this.remove(id, e => console.log('remove error data', id, 'error', e))
         else list.push(data)
         count -= 1
         if (count === 0) cb(null, list)
