1a0e71e0e148682f0c7d89800fe159f7b6cad3e8
lixinwei <xinwei.li@winsuntech.cn>
2017-12-21 10:10:37 +0800

    Enable copy and paste in Mac

--- node/lib/window.js
+++ node/lib/window.js
@@ -6,7 +6,7 @@ import mkdirp from 'mkdirp'
 import rimraf from 'rimraf'
 import fsUtils from 'nodejs-fs-utils'
 import Promise from 'bluebird'
-import { ipcMain, BrowserWindow, app, dialog } from 'electron'
+import { ipcMain, BrowserWindow, app, dialog, Menu } from 'electron'
 import { clearTasks } from './transmissionUpdate'
 import store from './store'
 
@@ -36,6 +36,28 @@ const initMainWindow = () => {
       experimentalFeatures: true
     }
   })
+
+  // Create the Application's main menu
+  const template = [{
+    label: 'Application',
+    submenu: [
+      { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+      { type: 'separator' },
+      { label: 'Quit', accelerator: 'Command+Q', click() { app.quit() } }
+    ] }, {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+      { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+      { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+      { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+    ] }
+  ]
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
   // window title
   _mainWindow.on('page-title-updated', (event) => {
