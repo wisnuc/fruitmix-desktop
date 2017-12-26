d3397397733042818cc70753651ba32b69b446fe
lixinwei <xinwei.li@winsuntech.cn>
2017-12-26 15:38:38 +0800

    fix bug of i18n in node/lib/newUpload

--- node/lib/configObserver.js
+++ node/lib/configObserver.js
@@ -1,4 +1,6 @@
 import i18n from 'i18n'
+import { app } from 'electron'
+
 import store from './store'
 import { getMainWindow } from './window'
 
@@ -11,6 +13,7 @@ const configObserver = () => {
     preUserConfig = store.getState().userConfig
     const config = global.configuration.getConfiguration()
     if (config.global && config.global.locales) i18n.setLocale(config.global.locales)
+    else i18n.setLocale(/zh/.test(app.getLocale()) ? 'zh-CN' : 'en-US')
     getMainWindow().webContents.send('CONFIG_UPDATE', config)
   }
 }
