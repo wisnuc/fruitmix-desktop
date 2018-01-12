7b3474361b511ebd62a5921be6f29a224ed58e7b
lixinwei <xinwei.li@winsuntech.cn>
2018-01-12 11:52:55 +0800

    refine drag-move message

---
+++ src/common/msg.jsx
@@ -0,0 +1,34 @@
+import i18n from 'i18n'
+
+/**
+ * @param { Object } node props
+ * @return { String } i18n message
+*/
+
+export function xcopyMsg(props) {
+  const getName = (node) => {
+    let name
+    switch (node.type) {
+      case 'home':
+        name = i18n.__('Home Title')
+        break
+      case 'public':
+        name = i18n.__('Public Drive')
+        break
+      case 'built-in':
+        name = i18n.__('Share Title')
+        break
+      default:
+        name = node.name
+    }
+    if (name.length > 20) return `${name.slice(0, 17)}...`
+    return name
+  }
+
+  const { type, srcDir, dstDir, entries } = props
+  const action = type === 'move' ? i18n.__('Moved') : i18n.__('Copied')
+  const srcName = getName(srcDir)
+  const dstName = getName(dstDir)
+  const target = entries.length > 1 ? i18n.__('%s Items Have Been', entries.length) : i18n.__('%s Has Been', getName(entries[0]))
+  return i18n.__('xcopyMsg {{action}} {{srcName}} {{dstName}} {{target}}', { action, srcName, dstName, target })
+}
