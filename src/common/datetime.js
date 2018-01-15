738d9e70cdfd6813d6f559776baeffb04b87a5a8
lixinwei <xinwei.li@winsuntech.cn>
2018-01-15 13:21:05 +0800

    add finish time of BT task

--- src/common/datetime.js
+++ src/common/datetime.js
@@ -27,3 +27,23 @@ export function toTimeSecond(datetime) {
   if (!dateStr || dateStr.length !== 6) return 0
   return (new Date(...dateStr).getTime())
 }
+
+/**
+ * @param { String } "1464946245000"
+ * @return { String } 2016-05-03 17:30
+*/
+
+export function formatTime(d) {
+  const date = new Date()
+  if (typeof d === 'number') {
+    date.setTime(d)
+  } else {
+    return '-'
+  }
+  const year = date.getFullYear()
+  const mouth = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  return `${year}-${mouth}-${day} ${hour}:${minute}`
+}
