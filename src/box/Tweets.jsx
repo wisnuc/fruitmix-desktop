e584138501bcfa78f98b3b2170c9ccdf6e98794a
lixinwei <xinwei.li@winsuntech.cn>
2018-02-01 11:41:17 +0800

    wrap Tweets with custom scrollBar

--- src/box/Tweets.jsx
+++ src/box/Tweets.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import i18n from 'i18n'
 import { CircularProgress, Paper, Avatar } from 'material-ui'
 import FileFolder from 'material-ui/svg-icons/file/folder'
+import { AutoSizer } from 'react-virtualized'
 import { parseTime } from '../common/datetime'
 import Thumb from '../file/Thumb'
 import DetailContainer from '../photo/DetailContainer'
-
-const curve = 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'
+import ScrollBar from '../common/ScrollBar'
 
 const imgUrl = 'http://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKQiahrEc8rUfECDTUq94WlcaNkTYTKzIKr3p5xgOPQO1juvtwO1YSUCHOPpup3oWo1AP3nOBVyPCw/132'
-
-const imgRD = 'https://picsum.photos/200?image='
 
 const overlayStyle = {
   top: 0,
@@ -56,102 +54,103 @@ class Tweets extends React.PureComponent {
     )
   }
 
-  renderTweets(tweet, i) {
-    const { ctime, comment, uuid, tweeter, list, index } = tweet
+  renderTweets({ index, key, style }) {
+    const { ctime, comment, uuid, tweeter, list } = this.props.tweets[index]
     const isSelf = this.props.guid === tweeter.id
     const isMedia = list && list.every(l => l.metadata)
     const isMany = list && list.length > 6
     const w = 120
     return (
-      <div
-        key={uuid}
-        style={{
-          width: '100%',
-          display: 'flex',
-          boxSizing: 'border-box',
-          padding: 24,
-          flexDirection: isSelf ? 'row-reverse' : 'row'
-        }}
-      >
-        <div style={{ width: 32 }} />
-        {/* Avatar */}
-        <div style={{ height: 40, width: 40 }}>
-          <Avatar src={imgUrl} size={40} />
-        </div>
-        <div style={{ width: 16 }} />
-        <div>
+      <div key={key} style={style}>
+        <div
+          style={{
+            padding: 24,
+            width: '100%',
+            display: 'flex',
+            boxSizing: 'border-box',
+            overflow: 'hidden',
+            flexDirection: isSelf ? 'row-reverse' : 'row'
+          }}
+        >
+          <div style={{ width: 32 }} />
+          {/* Avatar */}
+          <div style={{ height: 40, width: 40 }}>
+            <Avatar src={imgUrl} size={40} />
+          </div>
+          <div style={{ width: 16 }} />
           <div>
             <div style={{ height: 30, display: 'flex', alignItems: 'center', flexDirection: isSelf ? 'row-reverse' : 'row' }}>
               <div style={{ fontSize: 12, color: 'rgba(0,0,0,.54)', fontWeight: 500 }}>
-                { Array.from({ length: Math.random() * 16 })
-                  .map(() => String.fromCharCode(0x674e - Math.random() * 100)) }
+                { uuid.slice(0, 8) }
               </div>
               <div style={{ width: 8 }} />
               <div style={{ fontSize: 12, color: 'rgba(0,0,0,.54)' }}>
                 { parseTime(ctime) }
               </div>
             </div>
+            <div>
+              {
+                !list ?
+                  <Paper
+                    style={{
+                      fontSize: 20,
+                      display: 'flex',
+                      alignItems: 'center',
+                      borderRadius: 10,
+                      backgroundColor: '#FFF',
+                      padding: 10
+                    }}
+                  >
+                    { comment }
+                  </Paper>
+                  : isMedia ?
+                  <div style={{ width: 3 * w + 12, maxHeight: 400 }}>
+                    {
+                      list.map((l, i) => {
+                        console.log('list', l)
+                        const { sha256, filename } = l
+                        if (i > 5) return (<div key={sha256 + filename + i} />)
+                        // const float = i > 2 || !isSelf ? 'left' : 'right'
+                        const margin = isSelf && list.length < 3 && i === 0 ? `2px 2px 2px ${360 - list.length * 120 + 2}px` : 2
+                        return (
+                          <Paper
+                            key={sha256 + filename + i}
+                            onTouchTap={() => this.setState({ openDetail: true, list, seqIndex: i })}
+                            style={{ width: w, height: w, float: 'left', backgroundColor: '#FFF', margin, position: 'relative' }}
+                          >
+                            <Thumb
+                              digest={sha256}
+                              boxUUID={this.props.boxUUID}
+                              ipcRenderer={this.props.ipcRenderer}
+                              height={w}
+                              width={w}
+                            />
+                            {
+                              i === 5 && isMany &&
+                                <div style={Object.assign({ width: w, height: w }, overlayStyle)} >
+                                  { `+ ${list.length - 6}` }
+                                </div>
+                            }
+                          </Paper>
+                        )
+                      })
+                    }
+                  </div>
+                  :
+                  <Paper style={{ width: 3 * w + 12, height: 56, display: 'flex', alignItems: 'center', fontSize: 14 }}>
+                    <div style={{ width: 24, height: 24, display: 'flex', alignItems: 'center', backgroundColor: '#FF9100', padding: 16 }}>
+                      <FileFolder color="#FFF" />
+                    </div>
+                    <div style={{ width: 16 }} />
+                    <div style={{ maxWidth: 1 * w, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>
+                      { list[0].filename }
+                    </div>
+                    <div style={{ width: 4 }} />
+                    { list.length > 1 && i18n.__n('And Other %s Items', list.length)}
+                  </Paper>
+              }
+            </div>
           </div>
-          {
-            !list ?
-              <Paper
-                style={{
-                  fontSize: 20,
-                  display: 'flex',
-                  alignItems: 'center',
-                  borderRadius: 10,
-                  backgroundColor: '#FFF',
-                  padding: 10
-                }}
-              >
-                { comment }
-              </Paper>
-              : isMedia ?
-              <div style={{ width: 3 * w + 12, maxHeight: 400 }}>
-                {
-                  list.map((l, i) => {
-                    console.log('list', l)
-                    const { sha256, filename } = l
-                    if (i > 5) return (<div key={sha256 + filename + i} />)
-                    // const float = i > 2 || !isSelf ? 'left' : 'right'
-                    const margin = isSelf && list.length < 3 && i === 0 ? `2px 2px 2px ${360 - list.length * 120 + 2}px` : 2
-                    return (
-                      <Paper
-                        key={sha256 + filename + i}
-                        onTouchTap={() => this.setState({ openDetail: true, list, seqIndex: i })}
-                        style={{ width: w, height: w, float: 'left', backgroundColor: '#FFF', margin, position: 'relative' }}
-                      >
-                        <Thumb
-                          digest={sha256}
-                          boxUUID={this.props.boxUUID}
-                          ipcRenderer={this.props.ipcRenderer}
-                          height={w}
-                          width={w}
-                        />
-                        {
-                          i === 5 && isMany &&
-                            <div style={Object.assign({ width: w, height: w }, overlayStyle)} >
-                              { `+ ${list.length - 6}` }
-                            </div>
-                        }
-                      </Paper>
-                    )
-                  })
-                }
-              </div>
-              :
-              <Paper style={{ width: 3 * w + 12, height: 56, display: 'flex', alignItems: 'center', fontSize: 14 }}>
-                <div style={{ width: 24, height: 24, display: 'flex', alignItems: 'center', backgroundColor: '#FF9100', padding: 16 }}>
-                  <FileFolder color="#FFF" />
-                </div>
-                <div style={{ width: 16 }} />
-                <div style={{ maxWidth: 1 * w, overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>
-                  { list[0].filename }
-                </div>
-                <div style={{ width: 4 }} />
-                { list.length > 1 && i18n.__n('And Other %s Items', list.length)}
-              </Paper>
-          }
         </div>
       </div>
     )
@@ -167,16 +166,43 @@ class Tweets extends React.PureComponent {
 
   render() {
     console.log('render tweets', this.props)
+    const { tweets } = this.props
+    const rowCount = tweets && tweets.length || 0
+    let allHeight = 0
+    // 326, 202, 134
+    const hs = []
+    for (let i = 0; i < rowCount; i++) {
+      const t = tweets[i]
+      const isMedia = t.list && t.list.every(l => l.metadata)
+      const h = isMedia && t.list.length > 3 ? 326 : isMedia ? 202 : 134
+      hs.push(h)
+      allHeight += h
+    }
+    const rowHeight = ({ index }) => hs[index]
+
     return (
       <div
         ref={ref => (this.refContainer = ref)}
-        style={{ flexGrow: 1, height: '100%', backgroundColor: '#FAFAFA', overflow: 'auto' }}
+        style={{ flexGrow: 1, height: '100%', backgroundColor: '#FAFAFA', overflow: 'hidden' }}
       >
         {
-          !this.props.tweets ? this.renderLoading(32) : this.props.tweets.length > 0
-            ? this.props.tweets.map((t, i) => this.renderTweets(t, i)) : this.renderNoTweets()
+          !tweets ? this.renderLoading(32) : tweets.length > 0 ?
+            <AutoSizer>
+              {({ height, width }) => (
+                <ScrollBar
+                  style={{ outline: 'none' }}
+                  allHeight={allHeight}
+                  height={height}
+                  width={width - 2}
+                  rowCount={rowCount}
+                  rowHeight={rowHeight}
+                  rowRenderer={({ index, key, style }) => this.renderTweets({ index, key, style })}
+                />
+              )}
+            </AutoSizer>
+            : this.renderNoTweets()
         }
-        { this.props.tweets.length > 0 && <div style={{ height: 96 }} /> }
+        { tweets && tweets.length > 0 && <div style={{ height: 96 }} /> }
 
         {/* PhotoDetail */}
         <DetailContainer
