4864ab44ba6e056afa126b4cb4a0f47d72aa8219
lixinwei <xinwei.li@winsuntech.cn>
2018-03-07 16:02:16 +0800

    auto open devTools in dev mode

--- node/lib/window.js
+++ node/lib/window.js
@@ -30,7 +30,7 @@ const initMainWindow = () => {
     minWidth: 1366,
     minHeight: 768,
     title: 'WISNUC',
-    icon: path.join(global.rootPath, 'public/assets/images/icon.png'), // it doesn't work in devel mode
+    icon: path.join(global.rootPath, 'public/assets/images/icon.png'),
     webPreferences: {
       webSecurity: true, // set false to disable the same-origin policy
       experimentalFeatures: true
@@ -99,7 +99,8 @@ const initMainWindow = () => {
   })
 
   // debug mode
-  // _mainWindow.webContents.openDevTools()
+  if (process.env.NODE_ENV === 'dev') _mainWindow.webContents.openDevTools()
+
   // _mainWindow.maximize()
 
   if (global.BABEL_IS_RUNNING) { _mainWindow.loadURL(`file://${process.cwd()}/public/index.html`) } else { _mainWindow.loadURL(`file://${path.join(global.entryFileDir, '../public', 'index.html')}`) }
