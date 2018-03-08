c85356575feff13e7d2b66b42d3a2ed05ac40c14
lixinwei <xinwei.li@winsuntech.cn>
2018-03-08 14:04:01 +0800

    refine code

--- node/lib/login.js
+++ node/lib/login.js
@@ -1,13 +1,24 @@
 import { ipcMain } from 'electron'
 
+/*  Config will update 4 times:
+ *    init: before LOGIN, get all kind of app path, get lastDevice info
+ *    Login: update current device and user info
+ *    update last device: updateGlobalConfigAsync({ lastDevice })
+ *    update saved Token: updateUserConfigAsync(user.uuid, { saveToken: lastDevice.saveToken })
+ */
+
 ipcMain.on('LOGIN', (event, device, user) => {
   global.dispatch({ type: 'LOGIN', data: { device, user } })
 
   /* save last Device info */
   const lastDevice = device.mdev
-  global.configuration.updateGlobalConfigAsync({ lastDevice }).catch(e => console.log(e))
-  if (lastDevice.saveToken !== undefined) global.configuration.updateUserConfigAsync(user.uuid, { saveToken: lastDevice.saveToken })
-    .catch(e => console.log(e))
+  global.configuration.updateGlobalConfigAsync({ lastDevice })
+    .catch(e => console.error('updateGlobalConfigAsync error', e))
+
+  if (lastDevice.saveToken !== undefined) {
+    global.configuration.updateUserConfigAsync(user.uuid, { saveToken: lastDevice.saveToken })
+      .catch(e => console.error('updateUserConfigAsync error', e))
+  }
 })
 
 ipcMain.on('LOGOUT', () => {
@@ -15,5 +26,6 @@ ipcMain.on('LOGOUT', () => {
 })
 
 ipcMain.on('UPDATE_USER_CONFIG', (event, userUUID, data) => {
-  global.configuration.updateUserConfigAsync(userUUID, data).catch(e => console.log(e))
+  global.configuration.updateUserConfigAsync(userUUID, data)
+    .catch(e => console.error('updateUserConfigAsync error', e))
 })
