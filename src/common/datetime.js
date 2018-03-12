90acabb9eb8448c525d45a534e7037ee9e2cee7b
lixinwei <xinwei.li@winsuntech.cn>
2018-03-12 14:47:16 +0800

    refine timestamp in tweets

--- src/common/datetime.js
+++ src/common/datetime.js
@@ -51,10 +51,9 @@ export function formatTime(d) {
 }
 
 /**
- * @param { String } "1464946245000"
- * @param { String } "yesterday"
- * @return { String } 2016-05-03 17:30
-*/
+ * parseTime: return a short version timestamp
+ *
+ */
 
 export function parseTime(time) {
   const d = new Date()
@@ -80,6 +79,40 @@ export function parseTime(time) {
   else if (t < nt + 3600000) return i18n.__n('%d Hours Ago', Math.floor((t - nt) / 3600000))
   else if (t < nt) return i18n.__n('%d Minutes Ago', Math.floor((t - nt) / 60000) || 1)
   return i18n.__('Year {{year}} {{mon}} {{day}}', { year, mon, day })
+}
+
+/**
+ * parseTime: return the full timestamp
+*/
+
+export function parseFullTime(time) {
+  const d = new Date()
+  if (typeof time === 'number') d.setTime(time)
+  else return '-'
+
+  const n = new Date() // now
+  const [hour, min] = [d.getHours(), d.getMinutes()]
+    .map((a, i) => (i > 2 && a < 10 ? `0${a}` : a))
+
+  const [ye, mo, da] = [n.getFullYear(), n.getMonth() + 1, n.getDate()]
+
+  const [ly, ld] = [new Date(`${ye}`), new Date(`${ye}-${mo}-${da}`)]
+    .map(a => a.getTime()) // last year, last month, yesterday
+
+  const t = d.getTime() // target time
+  const nt = n.getTime() // now
+
+  const locale = i18n.getLocale()
+  const op = {}
+  op.year = { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false }
+  op.month = { month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false }
+  op.hour = { hour: '2-digit', minute: '2-digit', hour12: false }
+
+  if (t < ly) return d.toLocaleString(locale, op.year)
+  else if (t + 86400000 < ld) return d.toLocaleString(locale, op.month)
+  else if (t < ld) return i18n.__('Yesterday {{hour}} {{min}}', { hour, min })
+  else if (t < nt + 18000000) return d.toLocaleString(locale, op.hour)
+  return d.toLocaleString(locale, op.year)
 }
 
 export function formatMtime(mtime) {
