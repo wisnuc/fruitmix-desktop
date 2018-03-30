e12fd1871ad2f050d1b351f3d25eed432729eba3
lixinwei <xinwei.li@winsuntech.cn>
2018-03-30 18:12:38 +0800

    handle upload error in box [tmp]

--- src/box/Groups.jsx
+++ src/box/Groups.jsx
@@ -203,9 +203,14 @@ class Groups extends React.Component {
     }
 
     this.onFakeData = (event, args) => {
-      const { session, box, success, fakeList, raw } = args
+      const { session, box, success, fakeList, raw, error } = args
       if (!success) {
-        this.props.openSnackBar(i18n.__('Read Local Files Failed'))
+        let text = i18n.__('Read Local Files Failed')
+
+        if (error && error.code === 'ELARGE') text = i18n.__('Too Large File in Box Uploading')
+        else if (error && error.code === 'ENOTFILE') text = i18n.__('Unsupported File Type in Box Uploading')
+
+        this.props.openSnackBar(text)
       } else {
         const tweet = this.updateFakeTweet({ fakeList, box, raw })
         this.pushQueue(tweet.uuid)
